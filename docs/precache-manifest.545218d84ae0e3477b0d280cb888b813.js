26ba0302837bf11c394eedf89f27ed14daa9c753
Megharth <m.c.lakhataria@gmail.com>
2018-09-02 20:46:11 +0530

    production build

---
+++ docs/precache-manifest.545218d84ae0e3477b0d280cb888b813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b97e26f81d16774e4309",
+    "url": "/js/chunk-vendors.0b8dd0d3.js"
+  },
+  {
+    "revision": "359cba215c767e1a495d",
+    "url": "/js/app.75748bef.js"
+  },
+  {
+    "revision": "67ac1f42d821412d8dfb57c550fd3366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b97e26f81d16774e4309",
+    "url": "/css/chunk-vendors.a93545fb.css"
+  },
+  {
+    "revision": "359cba215c767e1a495d",
+    "url": "/css/app.91b26ba2.css"
+  }
+];
